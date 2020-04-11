0c57f1cc952b3d4c1a62f0339b39be74e881c981
Artem Moskvyak <moskvyak@gmail.com>
2020-04-11 22:07:24 +1000

    styling

--- src/components/SimpleChart/SimpleChart.tsx
+++ src/components/SimpleChart/SimpleChart.tsx
@@ -15,23 +15,61 @@ interface Props {
   data: object[];
   selectedCountries: any[];
 }
+class CustomizedXAxisTick extends React.PureComponent<any, any> {
+  render() {
+    const { x, y, payload } = this.props;
+    return (
+      <text
+        x={x + 20}
+        y={y}
+        dy={16}
+        textAnchor="end"
+        fill="#666"
+        fontSize="12px"
+      >
+        {payload.value}
+      </text>
+    );
+  }
+}
 
+class CustomizedYAxisTick extends React.PureComponent<any, any> {
+  render() {
+    const { x, y, payload } = this.props;
+    return (
+      <text
+        x={x}
+        y={y - 12}
+        dy={16}
+        textAnchor="end"
+        fill="#666"
+        fontSize="12px"
+      >
+        {payload.value}
+      </text>
+    );
+  }
+}
 const SimpleChart: React.FC<Props> = (props: Props) => {
   return (
     <ResponsiveContainer>
-      <LineChart
-        data={props.data}
-        >
+      <LineChart data={props.data}>
         {props.selectedCountries.map((selected: any, index: number) => {
           return (
-            <Line key={selected.id} type="monotone" dataKey={selected.name} stroke={colors[index]} dot={false}/>
+            <Line
+              key={selected.id}
+              type="monotone"
+              dataKey={selected.name}
+              stroke={colors[index]}
+              dot={false}
+            />
           );
         })}
         <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
-        <XAxis dataKey="id" />
-        <YAxis />
-        <Legend verticalAlign="top"/>
-        <Tooltip wrapperStyle={{zIndex: 1000}} />
+        <XAxis dataKey="id" tick={<CustomizedXAxisTick />} />
+        <YAxis tick={<CustomizedYAxisTick />} />
+        <Legend verticalAlign="top" height={40}/>
+        <Tooltip wrapperStyle={{ zIndex: 1000 }} />
       </LineChart>
     </ResponsiveContainer>
   );
