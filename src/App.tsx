67a5c754560a6c34c66a4091408e5b19c220e1dd
Artem Moskvyak <moskvyak@gmail.com>
2020-04-29 00:03:02 +1000

    finalize router

--- src/App.tsx
+++ src/App.tsx
@@ -6,13 +6,16 @@ import {
   BrowserRouter as Router,
   Switch,
   Route,
-  Redirect
+  Redirect,
+  NavLink
 } from 'react-router-dom';
 
 import { createStyles, Theme, makeStyles } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import { LocalizationProvider } from '@material-ui/pickers';
 import MomentUtils from '@material-ui/pickers/adapter/moment';
+import Drawer from '@material-ui/core/Drawer';
+import ListItem from '@material-ui/core/ListItem';
 
 import { GraphPage } from './pages/GraphPage';
 import { AboutPage } from './pages/AboutPage';
@@ -20,13 +23,23 @@ import { AboutPage } from './pages/AboutPage';
 const client = new ApolloClient({
   uri: process.env.REACT_APP_GRAPHQL
 });
+const drawerWidth = 120;
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     root: {
+      width: `calc(100% - ${drawerWidth}px)`,
+      marginLeft: drawerWidth,
       display: 'flex',
       flexDirection: 'column',
       height: '100%'
+    },
+    drawer: {
+      width: drawerWidth,
+      flexShrink: 0
+    },
+    drawerPaper: {
+      width: drawerWidth
     }
   })
 );
@@ -37,13 +50,40 @@ function App() {
     <ApolloProvider client={client}>
       <LocalizationProvider dateAdapter={MomentUtils}>
         <Router>
+          <Drawer
+            className={classes.drawer}
+            variant="permanent"
+            classes={{
+              paper: classes.drawerPaper
+            }}
+            anchor="left"
+          >
+            <ListItem
+              button
+              component={NavLink}
+              to="/graphs"
+              activeClassName="Mui-selected"
+              exact
+            >
+              Graphs
+            </ListItem>
+            <ListItem
+              button
+              component={NavLink}
+              to="/about"
+              activeClassName="Mui-selected"
+              exact
+            >
+              About
+            </ListItem>
+          </Drawer>
           <div className={classes.root}>
             <CssBaseline />
             <Switch>
-              <Route path="/graphs">
+              <Route path="/graphs" exact>
                 <GraphPage />
               </Route>
-              <Route path="/about">
+              <Route path="/about" exact>
                 <AboutPage />
               </Route>
               <Route path="/">
