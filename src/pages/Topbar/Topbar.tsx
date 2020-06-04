ef90f44dad065e531869b8fdd213516fc35a686e
Artem Moskvyak <moskvyak@gmail.com>
2020-06-04 21:49:13 +1000

    location title

--- src/pages/Topbar/Topbar.tsx
+++ src/pages/Topbar/Topbar.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import { Theme, makeStyles } from '@material-ui/core/styles';
-import { Link as RouterLink } from 'react-router-dom';
+import { Link as RouterLink, useLocation } from 'react-router-dom';
 import { AppBar, Toolbar, Hidden, IconButton } from '@material-ui/core';
 import MenuIcon from '@material-ui/icons/Menu';
 import logo from '../../virus.svg';
@@ -15,8 +15,17 @@ const useStyles = makeStyles((theme: Theme) => ({
   },
   signOutButton: {
     marginLeft: theme.spacing(1)
+  },
+  title: {
+    marginLeft: theme.spacing(3)
   }
 }));
+
+const routes: any = {
+  '/trends': 'Trends',
+  '/timeline': 'Timeline',
+  '/about': 'About'
+}
 
 interface Props {
   onSidebarOpen(): void;
@@ -24,7 +33,7 @@ interface Props {
 
 const Topbar: React.FC<Props> = (props: Props) => {
   const { onSidebarOpen } = props;
-
+  const location = useLocation();
   const classes = useStyles();
 
   return (
@@ -33,7 +42,14 @@ const Topbar: React.FC<Props> = (props: Props) => {
         <RouterLink to="/">
           <img width={40} height={40} alt="Logo" src={logo} />
         </RouterLink>
-        <div className={classes.flexGrow} />
+
+        <div className={classes.flexGrow}>
+          <Hidden lgUp>
+            <h1 className={classes.title}>
+              {routes[location.pathname]}
+            </h1>
+          </Hidden>
+        </div>
         <Hidden lgUp>
           <IconButton color="inherit" onClick={onSidebarOpen}>
             <MenuIcon />
